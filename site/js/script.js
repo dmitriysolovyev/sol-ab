85d06b4843237170232b5c1f156ce50272f98dba
dmitriysolovyev <dmitriysolovyev.dattatreya@yandex.ru>
2016-11-17 08:50:43 +0500

    структура сайта

--- site/js/script.js
+++ site/js/script.js
@@ -1,24 +1,26 @@
 var imgHead = [
-	            'images/1.png',
-	            'images/2.png',
-	            'images/3.png',
-	            'images/4.png'
+	            './images/1.png',
+	            './images/2.png',
+	            './images/3.png',
+	            './images/4.png'
 	        ], i=1;
 	    function csaHead(){
 
 	        if(i > (imgHead.length-1)){
-	            $('.csa-head').animate({'opacity':'0'},200,function(){
+	            $('.csa-head').animate({'opacity':'0'},300,function(){
 	                i=1;
-	                $('.csa-head').css({'background':'url('+imgHead[0]+')'});
+	                $('.csa-head').css({'background':'url('+imgHead[0]+') no-repeat center center',
+	            						'background-size': 'cover'});
 	            });
-	            $('.csa-head').animate({'opacity':'1'},200);
+	            $('.csa-head').animate({'opacity':'1'},300);
 	        }else{
-	            $('.csa-head').animate({'opacity':'0'},200,function(){
-	                $('.csa-head').css({'background':'url('+imgHead[i]+')'});
+	            $('.csa-head').animate({'opacity':'0'},300,function(){
+	                $('.csa-head').css({'background':'url('+imgHead[i]+') no-repeat center center',
+	            						'background-size': 'cover'});
 	                i++;
 	            });
-	            $('.csa-head').animate({'opacity':'1'},200);
+	            $('.csa-head').animate({'opacity':'1'},300);
 	        }
 	    }
 	    csaHead();
-	    var intervalCsaHead = setInterval(csaHead,800);
+	    var intervalCsaHead = setInterval(csaHead,4000);
