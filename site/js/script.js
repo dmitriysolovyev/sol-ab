cdc85edd20a4e3f4fc52f824cd64725705559682
dmitriysolovyev <dmitriysolovyev.dattatreya@yandex.ru>
2016-11-26 22:21:38 +0500

    animation, resize

--- site/js/script.js
+++ site/js/script.js
@@ -1,26 +1,66 @@
-var imgHead = [
-	            './images/1.png',
-	            './images/2.png',
-	            './images/3.png',
-	            './images/4.png'
-	        ], i=1;
-	    function csaHead(){
 
-	        if(i > (imgHead.length-1)){
-	            $('.csa-head').animate({'opacity':'0'},300,function(){
-	                i=1;
-	                $('.csa-head').css({'background':'url('+imgHead[0]+') no-repeat center center',
-	            						'background-size': 'cover'});
-	            });
-	            $('.csa-head').animate({'opacity':'1'},300);
-	        }else{
-	            $('.csa-head').animate({'opacity':'0'},300,function(){
-	                $('.csa-head').css({'background':'url('+imgHead[i]+') no-repeat center center',
-	            						'background-size': 'cover'});
-	                i++;
-	            });
-	            $('.csa-head').animate({'opacity':'1'},300);
-	        }
-	    }
-	    csaHead();
-	    var intervalCsaHead = setInterval(csaHead,4000);
+var imgHead = [ './images/01.png',
+	            './images/02.png',
+	            './images/03.png',
+	            './images/04.png'];
+
+function change_image() {
+	if(window.matchMedia('(max-width: 767px)').matches) {
+		console.log('(max-width: 767px)');
+		imgHead = [ './images/01_768.png',
+		            './images/02_768.png',
+		            './images/03_768.png',
+		            './images/04_768.png'];
+
+	} else if(window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+		console.log('(min-width: 768px) and (max-width: 991px)');
+		imgHead = [ './images/01_768.png',
+		            './images/02_768.png',
+		            './images/03_768.png',
+		            './images/04_768.png'];
+
+	} else if(window.matchMedia('(min-width: 992px) and (max-width: 1199px)').matches) {
+		console.log('(min-width: 992px) and (max-width: 1199px)');
+		imgHead = [ './images/01_992.png',
+		            './images/02_992.png',
+		            './images/03_992.png',
+		            './images/04_992.png'];
+
+	} else if(window.matchMedia('(min-width: 1200px)').matches) {
+		console.log('(min-width: 1200px)');
+		imgHead = [ './images/01.png',
+		            './images/02.png',
+		            './images/03.png',
+		            './images/04.png'];
+	}
+}
+
+change_image();
+$(window).resize(change_image);
+
+var i=1;
+function csaHead(){
+
+    if(i > (imgHead.length-1)){
+        $('.background-img').animate({'opacity':'0'},
+        							 3000,
+        							 function(){
+            i=1;
+            $('.background-img').css({'background':'url('+imgHead[0]+') no-repeat center center',
+        						'background-size': 'cover'});
+        });
+        $('.background-img').animate({'opacity':'1'},
+        							 1000);
+    }else{
+        $('.background-img').animate({'opacity':'0'},
+        							 3000,
+        							 function(){
+            $('.background-img').css({'background':'url('+imgHead[i]+') no-repeat center center',
+        						'background-size': 'cover'});
+            i++;
+        });
+        $('.background-img').animate({'opacity':'1'},1000);
+    }
+}
+csaHead();
+var intervalCsaHead = setInterval(csaHead,10000);
