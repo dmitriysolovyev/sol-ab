52457b90b04e2f27a21157207fb35720aad5ca93
dmitriysolovyev <dmitriysolovyev.dattatreya@yandex.ru>
2016-12-17 16:55:09 +0500

    переписал js

--- site/js/script.js
+++ site/js/script.js
@@ -1,63 +1,59 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-var imgHead = [];
+	var images_back = [];
 
-function change_image() {
-	if(window.matchMedia('(max-width: 767px)').matches) {
-		console.log('(max-width: 767px)');
-		imgHead = [ './images/01_768.png',
-		            './images/02_768.png',
-		            './images/03_768.png',
-		            './images/04_768.png'];
+	function resize_images() {
+		if(window.matchMedia('(max-width: 767px)').matches) {
+			console.log('(max-width: 767px)');
+			images_back = [ './images/01_768.png',
+			                './images/02_768.png',
+			                './images/03_768.png',
+			                './images/04_768.png'];
 
-	} else if(window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-		console.log('(min-width: 768px) and (max-width: 991px)');
-		imgHead = [ './images/01_768.png',
-		            './images/02_768.png',
-		            './images/03_768.png',
-		            './images/04_768.png'];
+		} else if(window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+			console.log('(min-width: 768px) and (max-width: 991px)');
+			images_back = [ './images/01_768.png',
+			                './images/02_768.png',
+			                './images/03_768.png',
+			                './images/04_768.png'];
 
-	} else if(window.matchMedia('(min-width: 992px) and (max-width: 1199px)').matches) {
-		console.log('(min-width: 992px) and (max-width: 1199px)');
-		imgHead = [ './images/01_992.png',
-		            './images/02_992.png',
-		            './images/03_992.png',
-		            './images/04_992.png'];
+		} else if(window.matchMedia('(min-width: 992px) and (max-width: 1199px)').matches) {
+			console.log('(min-width: 992px) and (max-width: 1199px)');
+			images_back = [ './images/01_992.png',
+			                './images/02_992.png',
+			                './images/03_992.png',
+			                './images/04_992.png'];
 
-	} else if(window.matchMedia('(min-width: 1200px)').matches) {
-		console.log('(min-width: 1200px)');
-		imgHead = [ './images/01_1200.png',
-	                './images/02_1200.png',
-	                './images/03_1200.png',
-	                './images/04_1200.png'];
+		} else if(window.matchMedia('(min-width: 1200px)').matches) {
+			console.log('(min-width: 1200px)');
+			images_back = [ './images/01_1200.png',
+		                    './images/02_1200.png',
+		                    './images/03_1200.png',
+		                    './images/04_1200.png'];
+		}
 	}
-}
 
-change_image();
-$(window).resize(change_image);
+	var indx = 0;
+	function background_next() {
+	           
+		$('.background-img').css({'background':'url(' + images_back[indx] +
+	            	                              ') no-repeat center center',
+	        					  'background-size': 'cover'});
+	}
 
-var i=1;
-function csaHead(){
+	function change_image(){
 
-    if(i > (imgHead.length-1)){
-        $('.background-img').animate({'opacity':'0'},
-        							 3000,
-        							 function(){
-            i=1;
-            $('.background-img').css({'background':'url('+imgHead[0]+') no-repeat center center',
-        						'background-size': 'cover'});
-        });
-        $('.background-img').animate({'opacity':'1'},
-        							 1000);
-    }else{
-        $('.background-img').animate({'opacity':'0'},
-        							 3000,
-        							 function(){
-            $('.background-img').css({'background':'url('+imgHead[i]+') no-repeat center center',
-        						'background-size': 'cover'});
-            i++;
-        });
-        $('.background-img').animate({'opacity':'1'},1000);
-    }
-}
-// csaHead();
-var intervalCsaHead = setInterval(csaHead,10000);
+	    $('.background-img').animate({'opacity':'0'},
+	            					 2500,
+	        					     background_next);
+	    $('.background-img').animate({'opacity':'1'},
+	        						 1200);
+	    indx = ++indx % images_back.length;
+	}
+
+	$(window).resize(resize_images);
+	resize_images();
+
+	change_image();
+	var interval = setInterval(change_image,10000);
+});
