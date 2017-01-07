ac6346f11a641936444edce06cd566794b3e9745
dmitriysolovyev <dmitriysolovyev.dattatreya@yandex.ru>
2017-01-07 22:58:51 +0500

    на хостинг

--- site/js/script.js
+++ site/js/script.js
@@ -4,28 +4,28 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 	function resize_images() {
 		if(window.matchMedia('(max-width: 767px)').matches) {
-			console.log('(max-width: 767px)');
+			// console.log('(max-width: 767px)');
 			images_back = [ './images/01_768.png',
 			                './images/02_768.png',
 			                './images/03_768.png',
 			                './images/04_768.png'];
 
 		} else if(window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-			console.log('(min-width: 768px) and (max-width: 991px)');
+			// console.log('(min-width: 768px) and (max-width: 991px)');
 			images_back = [ './images/01_768.png',
 			                './images/02_768.png',
 			                './images/03_768.png',
 			                './images/04_768.png'];
 
 		} else if(window.matchMedia('(min-width: 992px) and (max-width: 1199px)').matches) {
-			console.log('(min-width: 992px) and (max-width: 1199px)');
+			// console.log('(min-width: 992px) and (max-width: 1199px)');
 			images_back = [ './images/01_992.png',
 			                './images/02_992.png',
 			                './images/03_992.png',
 			                './images/04_992.png'];
 
 		} else if(window.matchMedia('(min-width: 1200px)').matches) {
-			console.log('(min-width: 1200px)');
+			// console.log('(min-width: 1200px)');
 			images_back = [ './images/01_1200.png',
 		                    './images/02_1200.png',
 		                    './images/03_1200.png',
